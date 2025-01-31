c956c8cd6accac59c22405df8d9bc55f64b21b9f
Arianna Dragoni <ariannadragoni@Ariannas-MacBook-Pro.local>
2025-01-31 16:01:27 +0100

    log

--- log.go
+++ log.go
@@ -8,22 +8,17 @@ import (
 )
 
 func MyLog(data string) {
-	// URL del server Axum (modifica l'indirizzo IP della tua macchina virtuale)
-	url := "http://10.168.89.115:3000/" // Modifica l'IP e il percorso in base al tuo caso
+	url := "http://192.168.64.1:3000/"
 
-	// Corpo della richiesta (puoi inviare la stringa come JSON o come corpo di una richiesta normale)
-	body := []byte(data) // Convertiamo la stringa in un array di byte
+	body := []byte(data)
 
-	// Creiamo una richiesta POST
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Impostiamo l'header Content-Type
 	req.Header.Set("Content-Type", "text/plain")
 
-	// Inviamo la richiesta
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -31,6 +26,5 @@ func MyLog(data string) {
 	}
 	defer resp.Body.Close()
 
-	// Stampiamo il codice di stato della risposta
 	fmt.Println("Response Status:", resp.Status)
 }
