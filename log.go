a6d9a3726bbd6cd450887c0a220556c94f8e0471
Arianna Dragoni <ariannadragoni@Ariannas-MacBook-Pro.local>
2025-02-03 18:08:19 +0100

    new log function

--- log.go
+++ log.go
@@ -50,3 +50,25 @@ func MyNodeLog(node string, data string) {
 
 	fmt.Println("Response Status:", resp.Status)
 }
+
+func GeneralLog(data string) {
+	url := "http://192.168.64.1:3000/"
+
+	body := []byte(data)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "text/plain")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status:", resp.Status)
+}
